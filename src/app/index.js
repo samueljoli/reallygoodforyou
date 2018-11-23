b4085dab297dcba08c61084604910778d236304c
Samuel Joli <sjoli@ny-sjolimbp2.home>
2018-11-23 16:11:50 -0500

    Add cultclassic redirect

--- src/app/index.js
+++ src/app/index.js
@@ -18,6 +18,7 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/contact" component={Contact} />
+          <Route exact path="/cultclassic" component={() => window.location = 'https://cultclassic2.splashthat.com/'} />
         </Container>
       </Router>
     );
